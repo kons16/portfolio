c2258e6a778e2a45862b3b32a392a052b9fab42c
kons16 <river.field.1126@gmail.com>
2019-11-01 16:08:55 +0900

    fix icon-link

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,14 +4,16 @@ class Top extends React.Component {
   render() {
     return (
       <div>
-        <div id="top-image">
-            <a href="/" id="top-to-link"><img src={`${process.env.PUBLIC_URL}/debudori.png`} id="mylogo" /></a>
-            <h1 id="myname">Kono Shinji</h1>
-            <a href="https://github.com/kons16" target="_blank">
-              <img src={`${process.env.PUBLIC_URL}/octcat64.png`} id="github-icon" />
-            </a>
-            <h3 id="career">茨城大学工学部情報工学科4年</h3>
-        </div>
+        <a href="/">
+          <img src={`${process.env.PUBLIC_URL}/debudori.png`} id="mylogo" />
+        </a>
+        <h1 id="myname">
+          Kono Shinji
+          <a href="https://github.com/kons16" target="_blank">
+            <img src={`${process.env.PUBLIC_URL}/octcat64.png`} id="github-icon" />
+          </a>
+        </h1>
+        <h3 id="career">茨城大学工学部情報工学科4年</h3>
       </div>
     );
   }
