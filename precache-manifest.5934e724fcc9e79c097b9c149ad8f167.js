6a21c736dba548a6d1b506f1c4029575100bfb07
kons16 <ks1216r@gmail.com>
2019-12-05 16:55:05 +0900

    Updates

---
+++ precache-manifest.5934e724fcc9e79c097b9c149ad8f167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb4b5c716047fc5241292156cd0d500",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a49cd6585a2daee58bc",
+    "url": "/portfolio/static/css/main.bb969d0a.chunk.css"
+  },
+  {
+    "revision": "c9ef5ab9d617176660e7",
+    "url": "/portfolio/static/js/2.dc4d5b83.chunk.js"
+  },
+  {
+    "revision": "7a49cd6585a2daee58bc",
+    "url": "/portfolio/static/js/main.5af6a152.chunk.js"
+  },
+  {
+    "revision": "67b6cc4ae1dda2cb6f68",
+    "url": "/portfolio/static/js/runtime-main.7934be05.js"
+  }
+]);
