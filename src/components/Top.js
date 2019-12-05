629dd4370afec8dc3c45af6069b39fdf47cd4e4b
kons16 <ks1216r@gmail.com>
2019-12-05 17:13:47 +0900

    fix about

--- src/components/Top.js
+++ src/components/Top.js
@@ -11,7 +11,6 @@ class Top extends React.Component {
             <img src={`${process.env.PUBLIC_URL}/octcat64.png`} id="github-icon" alt="github-icon"/>
           </a>
         </h1>
-        <h3 id="career">茨城大学工学部情報工学科4年</h3>
       </div>
     );
   }
