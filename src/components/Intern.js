e6cd54e985c0768fccff33884ac6fe6c6d0d8404
kons16 <ks1216r@gmail.com>
2020-03-21 14:51:43 +0900

    レイアウト調整

--- src/components/Intern.js
+++ src/components/Intern.js
@@ -12,29 +12,29 @@ class Intern extends React.Component {
               <a id="experiment-li-company">ユニキャスト 就業型インターン</a>
               <a id="experiment-li"></a>
               <a id="experiment-li-detail">
-                C#(ASP.NET)を使った企業受け付けアプリのサーバーサイド開発.
-                Railsを利用したWebサイトの修正.
+                C#(ASP.NET)を使った企業受け付けアプリのサーバーサイド開発。
+                Railsを利用したWebサイトの修正。
               </a>
 
               <a id="experiment-li-day">2019年5月</a>
               <a id="experiment-li-company">ミクシィ git challenge #11</a>
               <a id="experiment-li"></a>
               <a id="experiment-li-detail">
-                gitに関連する問題を解いていく競技型のイベントに<br/>参加しました.
+                gitに関連する問題を解いていく競技型のイベントに参加しました。
               </a>
 
               <a id="experiment-li-day">2019年9月</a>
               <a id="experiment-li-company">フューチャー EngineerCamp</a>
               <a id="experiment-li"></a>
               <a id="experiment-li-detail">
-                フロントはJavaScript, サーバーサイドはJava(SpringBoot,uroboroSQL)を利用したWebアプリの開発.
+                フロントはJavaScript、サーバーサイドはJava(SpringBoot,uroboroSQL)を利用したWebアプリ開発。
               </a>
 
               <a id="experiment-li-day">2020年2月〜3月</a>
               <a id="experiment-li-company">ドワンゴ 就業型インターン</a>
               <a id="experiment-li"></a>
               <a id="experiment-li-detail">
-                kotlinを使ったドワンゴジェイピー(Androidアプリ)の開発.
+                kotlinを使ったドワンゴジェイピー(Androidアプリ)の開発。
               </a>
 
               <a id="experiment-li-day">2020年3月</a>
