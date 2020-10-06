0435697f7755e5c5a633155fd1cc52d5f46fe7b9
kons16 <ks1216r@gmail.com>
2020-10-06 14:49:44 +0900

    worksを変更

--- src/components/Works.js
+++ src/components/Works.js
@@ -15,7 +15,7 @@ class Works extends React.Component {
               </div>
               <div id="works-li-detail">
                 会話を続けるためのコツは「深堀り」と「質問」が大切だと言われています.
-                このConcierge(コンシェルジュ)は1対1のチャットアプリに自動で質問生成を行う機能がついているため,
+                この Concierge(コンシェルジュ) は1対1のチャットアプリに自動で質問生成を行う機能がついているため,
                 会話を簡単に続けることができます.
                 <a href="https://github.com/kons16/Concierge" target="_blank" rel="noreferrer noopener">[GitHub]</a>
               </div>
@@ -25,41 +25,39 @@ class Works extends React.Component {
              </div>
               <div id="works-li"></div>
 
-              <div id="works-li-title">ClassDeru</div>
+              <div id="works-li-title">meibun</div>
               <div id="works-li-icon">
-                <img src={`${process.env.PUBLIC_URL}/classderu.png`} id="works-icon" alt="works-icon" />
+                <img src={`${process.env.PUBLIC_URL}/meibun_top.png`} id="works-icon" alt="works-icon" />
               </div>
               <div id="works-li-detail">
-                ClassDeru(クラスデル)は大学の授業における出席を簡単に取ることのできるWebアプリです.
-                学生が1クリックで授業に出席でき, 教員の方は誰が出席しているのか画面上で確認できる他, 
-                CSVで出席者一覧をダウンロードできます. <br />
-                <a href="https://github.com/kons16/ClassDeru" target="_blank" rel="noreferrer noopener">[GitHub]</a>
+                本に登場する名文を記録できるWebアプリです。バックエンドは Go+DDD構成、フロントは React+TypeScript で開発しました。
+                <a href="https://github.com/kons16/meibun" target="_blank" rel="noreferrer noopener">[GitHub]</a>
               </div>
               <div id="works-li"></div>
               <div id="works-li-use">
-                使用した技術: Laravel, SQLite
-              </div>
+                使用した技術: Go, MySQL, Docker, React, TypeScript
+             </div>
               <div id="works-li"></div>
 
-              <div id="works-li-title">アンケートアプリ</div>
+              <div id="works-li-title">タスク管理アプリ</div>
               <div id="works-li-icon">
-                <img src={`${process.env.PUBLIC_URL}/q-kekka.png`} id="works-icon" alt="works-icon" />
+                <img src={`${process.env.PUBLIC_URL}/labord.png`} id="works-icon" alt="works-icon" />
               </div>
               <div id="works-li-detail">
-                Reactの勉強のために作成したアンケートアプリです. 誰でもアンケートの作成・回答が可能で,
-                アンケート結果の保存とホスティングにFirebaseを利用しています. <br />
-                <a href="https://github.com/kons16/questionnaire" target="_blank" rel="noreferrer noopener">[GitHub]</a>
+                React+AWS でサーバーレスなタスク管理アプリを開発しました。タスクを投稿すると Lambda から slack にタスク内容が投稿されるようになっています。
+                また CircleCI で S3 にホスティングさせています。
+                <a href="https://github.com/kons16/Labord" target="_blank" rel="noreferrer noopener">[GitHub]</a>
               </div>
               <div id="works-li"></div>
               <div id="works-li-use">
-                使用した技術: React, Firebase(Firestore, Hosting)
+                使用した技術: React, CircleCI, <br/>AWS(Route53, CloudFront, S3, Lambda, DynamoDB)
               </div>
               <div id="works-li-icon"></div>
 
               <div id="works-li-title">ポートフォリオ</div>
               <div id="works-li"></div>
               <div id="works-li-detail">
-                このサイトです.<br />Reactを使用しています.
+                このサイトです.<br />React を使用しています.
                 <a href="https://github.com/kons16/portfolio" target="_blank" rel="noreferrer noopener">[GitHub]</a>
               </div>
               <div id="works-li"></div>
